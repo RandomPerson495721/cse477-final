c9f6bdece1609c89e335bf585ed726b944e27253
James Partin <jamesaustinpartin@gmail.com>
2025-02-16 15:51:45 -0500

    Enhance piano project with new CSS styles, HTML updates, and JavaScript functionality for interactive features

--- Homework-1/flask_app/static/piano/js/piano.js
+++ Homework-1/flask_app/static/piano/js/piano.js
@@ -1,21 +1,25 @@
-const sound = {65:"http://carolinegabriel.com/demo/js-keyboard/sounds/040.wav",
-    87:"http://carolinegabriel.com/demo/js-keyboard/sounds/041.wav",
-    83:"http://carolinegabriel.com/demo/js-keyboard/sounds/042.wav",
-    69:"http://carolinegabriel.com/demo/js-keyboard/sounds/043.wav",
-    68:"http://carolinegabriel.com/demo/js-keyboard/sounds/044.wav",
-    70:"http://carolinegabriel.com/demo/js-keyboard/sounds/045.wav",
-    84:"http://carolinegabriel.com/demo/js-keyboard/sounds/046.wav",
-    71:"http://carolinegabriel.com/demo/js-keyboard/sounds/047.wav",
-    89:"http://carolinegabriel.com/demo/js-keyboard/sounds/048.wav",
-    72:"http://carolinegabriel.com/demo/js-keyboard/sounds/049.wav",
-    85:"http://carolinegabriel.com/demo/js-keyboard/sounds/050.wav",
-    74:"http://carolinegabriel.com/demo/js-keyboard/sounds/051.wav",
-    75:"http://carolinegabriel.com/demo/js-keyboard/sounds/052.wav",
-    79:"http://carolinegabriel.com/demo/js-keyboard/sounds/053.wav",
-    76:"http://carolinegabriel.com/demo/js-keyboard/sounds/054.wav",
-    80:"http://carolinegabriel.com/demo/js-keyboard/sounds/055.wav",
-    186:"http://carolinegabriel.com/demo/js-keyboard/sounds/056.wav"};
+const sound = {
+    65:"https://carolinegabriel.com/demo/js-keyboard/sounds/040.wav",
+    87:"https://carolinegabriel.com/demo/js-keyboard/sounds/041.wav",
+    83:"https://carolinegabriel.com/demo/js-keyboard/sounds/042.wav",
+    69:"https://carolinegabriel.com/demo/js-keyboard/sounds/043.wav",
+    68:"https://carolinegabriel.com/demo/js-keyboard/sounds/044.wav",
+    70:"https://carolinegabriel.com/demo/js-keyboard/sounds/045.wav",
+    84:"https://carolinegabriel.com/demo/js-keyboard/sounds/046.wav",
+    71:"https://carolinegabriel.com/demo/js-keyboard/sounds/047.wav",
+    89:"https://carolinegabriel.com/demo/js-keyboard/sounds/048.wav",
+    72:"https://carolinegabriel.com/demo/js-keyboard/sounds/049.wav",
+    85:"https://carolinegabriel.com/demo/js-keyboard/sounds/050.wav",
+    74:"https://carolinegabriel.com/demo/js-keyboard/sounds/051.wav",
+    75:"https://carolinegabriel.com/demo/js-keyboard/sounds/052.wav",
+    79:"https://carolinegabriel.com/demo/js-keyboard/sounds/053.wav",
+    76:"https://carolinegabriel.com/demo/js-keyboard/sounds/054.wav",
+    80:"https://carolinegabriel.com/demo/js-keyboard/sounds/055.wav",
+    59:"https://carolinegabriel.com/demo/js-keyboard/sounds/056.wav"
+};
 
+let history = []
+let awoken = false;
 
 let whiteKeys = document.getElementsByClassName('white-key');
 let blackKeys = document.getElementsByClassName('black-key');
@@ -47,9 +51,11 @@ function hideNote() {
 // Function to handle the key press event
 function keyPress(event) {
     let key = document.getElementById(event.key.toUpperCase());
-    if (key === null) {
+    if (key === null || awoken) {
         return;
     }
+    var audio = new Audio(sound[event.keyCode]);
+    audio.play();
     if (key.className === 'white-key') {
         key.style.backgroundColor = 'rgba(255,255,255,0.5)';
     } else {
@@ -60,13 +66,32 @@ function keyPress(event) {
 // Function to handle the key release event
 function keyRelease(event) {
     let key = document.getElementById(event.key.toUpperCase());
-    if (key === null) {
+    if (key === null || awoken) {
         return;
     }
+
+    history.push(event.key.toUpperCase());
+    if (history.length > 8) {
+        history.shift();
+    }
+
+    console.log(history.join(''));
+
+
+
     if (key.className === 'white-key') {
         key.style.backgroundColor = 'rgba(255,255,255,1)';
     } else {
         key.style.backgroundColor = 'rgba(0,0,0,1)';
+    }
+
+    if (history.join('') === "WESEEYOU")
+    {
+        document.getElementById('greatOldOne').classList.replace('hidden', 'visible');
+        awoken = true;
+        /* Play creepy music */
+        var audio = new Audio("https://orangefreesounds.com/wp-content/uploads/2020/09/Creepy-piano-sound-effect.mp3?_=1");
+        audio.play();
     }
 }
 
